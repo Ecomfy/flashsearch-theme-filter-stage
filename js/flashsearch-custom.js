160d0d2dd839aa911df55b0a0408b12cd515f5cd
Andrew Bien <andrew.bien.bk@gmail.com>
2023-09-21 01:21:36 +0000

    Release da2d00f9

--- js/flashsearch-custom.js
+++ js/flashsearch-custom.js
@@ -2008,6 +2008,7 @@ flashsearch.searchResultsTemplates = {
         :compare-at-price-max="product.compareAtPriceMax"
         :enable-compare-at-price="enableCompareAtPrice"
         data-testid-prefix="sr"
+        :product="product"
       />
       <!-- product color -->
       <fs-product-colors
